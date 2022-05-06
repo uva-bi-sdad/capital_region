8e86cf55368bceacc4928135bcc7f57815df42a2
Micah <micah.iserman@gmail.com>
2022-05-06 10:21:44 -0400

    adds back neighborhood demographics

--- docs/functions/api.js
+++ docs/functions/api.js
@@ -3,8 +3,8 @@ const DataHandler = require('../data_handler.min.js'),
   data = new DataHandler(require('../settings.json'), void 0, {
     block_group: require('../block_group.json'),
     tract: require('../tract.json'),
-    county: require('../county.json'),
     neighborhood: require('../neighborhood.json'),
+    county: require('../county.json'),
   })
 module.exports.handler = async function (event) {
   return data.export(event.queryStringParameters)
